962ff62059ffdd9d56aafc056cc91765a65abc94
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-06 05:25:57 +0530

    added register user mutation

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,10 +1,20 @@
+import Bcyrpt from "bcrypt"
+
 export default {
-  Query: {
-    getUser: (parent, { id }, { models }) =>
-      models.User.findOne({ where: { id } }),
-    allUsers: (parent, args, { models }) => models.User.findAll()
-  },
-  Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args)
-  }
+    Query: {
+        getUser: (parent, { id }, { models }) =>
+            models.User.findOne({ where: { id } }),
+        allUsers: (parent, args, { models }) => models.User.findAll()
+    },
+    Mutation: {
+        register: async (parent, {password, ...otherArgs}, { models }) => {
+            try{
+                const hashedPassword = await Bcyrpt.hash(password, 12);
+                await models.User.create({...otherArgs, password: hashedPassword});
+                return true;
+            }catch(err){
+                return false
+            }
+        }
+    }
 };
