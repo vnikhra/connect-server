0275fc07a57f0e05c5e7d1a1d0adc3a4f6e34879
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-06 06:28:25 +0530

    Added data validation for user creation

--- models/users.js
+++ models/users.js
@@ -2,14 +2,62 @@ export default (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     username: {
       type: DataTypes.STRING,
-      unique: true
+      unique: {
+        args: true,
+        msg: "Username already exists !!!"
+      },
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Username cannot be empty"
+        },
+        notEmpty: {
+          args: true,
+          msg: "Username cannot be empty"
+        },
+        isAlphanumeric: {
+          args: true,
+          msg: "Username can be only alphabets and numbers"
+        },
+        len: {
+          args: [3, 25],
+          msg: "Username must be between 3 to 25 characters"
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
-      unique: true
+      unique: {
+        args: true,
+        msg: "Email already exists !!!"
+      },
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Email cannot be empty"
+        },
+        notEmpty: {
+          args: true,
+          msg: "Email cannot be empty"
+        },
+        isEmail: {
+          args: true,
+          msg: "Mot a valid Email"
+        }
+      }
     },
     password: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Password cannot be empty"
+        },
+        notEmpty: {
+          args: true,
+          msg: "Password cannot be empty"
+        }
+      }
     }
   });
 
