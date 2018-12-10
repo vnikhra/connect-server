4bd2707db0ead8620757099dccd11e4e642c0713
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-10 10:17:08 +0530

    Permissions and auth

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,19 +1,22 @@
 import { formatErrors } from "../utils/formatErrors";
+import { requiresAuth } from "../utils/permissions";
 
 export default {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      try {
-        await models.Team.create({ ...args, owner: user.id });
-        return {
-          ok: true
-        };
-      } catch (err) {
-        return {
-          ok: false,
-          errors: formatErrors(err)
-        };
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            ok: true
+          };
+        } catch (err) {
+          return {
+            ok: false,
+            errors: formatErrors(err)
+          };
+        }
       }
-    }
+    )
   }
 };
