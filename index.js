e47c46360d6cea8fca7bdb0f9c2d0fc00bb2a41c
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-16 21:18:15 +0530

    setup graphql supscriptions

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import models from "./models";
 import path from "path";
 import express from "express";
+import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
 import { ApolloServer, gql } from "apollo-server-express";
 import { addUser } from "./utils/addUser";
 import cors from "cors";
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 
 export const SECRET = "ahdwkhdh21eh2rkjfh2ih432ytr8ufhw";
 export const SECRET2 = "l73973;qkd;qkfd;qjk'1i291iqfkcoekflwejf;l";
@@ -16,6 +20,11 @@ const app = express();
 const resolvers = mergeResolvers(
   fileLoader(path.join(__dirname, "./resolvers"))
 );
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
 
 const server = new ApolloServer({
   typeDefs,
@@ -33,8 +42,23 @@ app.use(cors());
 
 server.applyMiddleware({ app });
 
-models.sequelize.sync({force: false}).then(() => {
-  app.listen(4000, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  );
+const httpServer = createServer(app);
+
+models.sequelize.sync({ force: false }).then(() => {
+  httpServer.listen(4000, () => {
+    SubscriptionServer.create(
+      {
+        schema,
+        execute,
+        subscribe
+      },
+      {
+        server: httpServer,
+        path: "/subscriptions"
+      }
+    );
+    console.log(
+      `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+    );
+  });
 });
