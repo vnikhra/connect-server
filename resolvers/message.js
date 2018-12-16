4b8385604739c705a04062a5e5dff8925834048d
Vibhor Nikhra <vibhor.nikhra@gmail.com>
2018-12-16 20:09:35 +0530

    1. Added auth on message query and mutation 2. added proper message query.

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,15 +1,29 @@
+import { requiresAuth } from "../utils/permissions";
+
 export default {
+  Message: {
+    user: ({ userId }, args, { models }) =>
+      models.User.findOne({ where: { id: userId } }, { raw: true })
+  },
   Query: {
-    messages: async (parent, { channelId }, { models, user }) => []
+    messages: requiresAuth.createResolver(
+      async (parent, { channelId }, { models, user }) =>
+        models.Message.findAll(
+          { order: [["createdAt", "ASC"]], where: { channelId } },
+          { raw: true }
+        )
+    )
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (err) {
-        return false;
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Message.create({ ...args, userId: user.id });
+          return true;
+        } catch (err) {
+          return false;
+        }
       }
-    }
+    )
   }
 };
